06472649aa7061327baafdeb6e13d7f4d28b6a44
mayank chouhan <chouhan.mayank@gmail.com>
2024-05-02 18:06:44 +0530

    Making changes to test CI CD

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Congratulations, Showing ECS demo');
+    res.send('Congratulations, Showing ECS & CLI demo');
 });
 
 app.get('/health', (req, res) => {
     console.log('App is healthy');;
-    res.status(200).send('App is healthy');
+    res.status(200).send(' CLI &  ECS App is healthy');
 });
 
 app.listen(port, () => {
-    console.log(`ECS demo app Listening at ${port}`);
+    console.log(`ECS & CLI demo app Listening at ${port}`);
 });
